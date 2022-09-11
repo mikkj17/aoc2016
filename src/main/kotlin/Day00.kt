9fbd95583cec77ab9f40bc68a13135716512b86c
Mikkel <mikkel@housekeepr.dk>
2022-09-11 13:38:32 +0200

    add template

---
+++ src/main/kotlin/Day00.kt
@@ -0,0 +1,15 @@
+import java.io.File
+
+private fun partOne(inp: String): Int {
+    return 0
+}
+
+private fun partTwo(inp: String): Int {
+    return 0
+}
+
+fun main() {
+    val inp = File("src/main/resources/day00.txt").readText().trim()
+    println(partOne(inp))
+    println(partTwo(inp))
+}
